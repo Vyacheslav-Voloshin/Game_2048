d3849d35b5943ea0ae428bc53874c00a0288e5fe
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-07-30 20:24:13 +0300

    Created play field in class Model

--- src/game/Model.java
+++ src/game/Model.java
@@ -2,6 +2,19 @@ package game;
 /*
  Клас міститиме ігрову логіку та зберігатиме
 ігрове поле.
+Клас буде відповідальний за всі маніпуляції, що проводяться
+ з ігровим полем.
  */
 public class Model {
+    private static final int FIELD_WIDTH = 4; //константа відповідаюча за ширину поля
+    private Tile [][] gameTiles; //
+
+    public Model() {
+        this.gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
